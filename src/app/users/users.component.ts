11650af3995206b1b05aa1a0d2b77a7825357c7e
Aria <ariaman2002@gmail.com>
2023-09-19 17:07:44 +0330

    format code

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { FormControl, Validators } from '@angular/forms';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import Swal from 'sweetalert2';
@@ -14,57 +14,68 @@ import { Router } from '@angular/router';
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss'],
-  standalone:true,
-  imports: [MatFormFieldModule, MatInputModule, MatIconModule,
-    MatCardModule,MatButtonModule,FormsModule,ReactiveFormsModule],
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
-
 export class UsersComponent {
+  userNameControl = new FormControl('', [
+    Validators.pattern('^[a-z0-9_-]{4,15}$'),
+  ]);
+  emailControl = new FormControl('', [
+    Validators.pattern('[^@ \t\r\n]+@[^@ \t\r\n]+.[^@ \t\r\n]+'),
+  ]);
+  lastNameValue = new FormControl('', [Validators.minLength(4)]);
+  ageValue = new FormControl('', [Validators.required]);
+  count: string = '0';
 
-   userNameControl = new FormControl('',[Validators.pattern('^[a-z0-9_-]{4,15}$')])
-   emailControl = new FormControl('',[Validators.pattern('[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+')])
-   lastNameValue = new FormControl('',[Validators.minLength(4)])
-   ageValue = new FormControl('',[Validators.required])
-   count : string = '0'
+  constructor(private facade: FacadeService, private route: Router) {}
 
-   constructor(private facade: FacadeService, private route: Router){}
-
-   clickHandler(){
-
-    if (!this.emailControl.valid || !this.userNameControl.valid || !this.ageValue.valid || !this.lastNameValue) {
-
+  clickHandler() {
+    if (
+      !this.emailControl.valid ||
+      !this.userNameControl.valid ||
+      !this.ageValue.valid ||
+      !this.lastNameValue
+    ) {
       Swal.fire({
         icon: 'error',
         title: 'Oops...',
         text: 'Something went wrong!',
-      })
-
-    }else{
-
+      });
+    } else {
       const myObj = {
-        id: Math.floor(Math.random()*6),
+        id: Math.floor(Math.random() * 6),
         name: this.userNameControl.value,
         lastName: this.lastNameValue.value,
         email: this.emailControl.value,
-        age: this.ageValue.value
-      }
+        age: this.ageValue.value,
+      };
 
-      this.facade.dispatchAdd(myObj)
+      this.facade.dispatchAdd(myObj);
 
       Swal.fire({
-        title: 'Your Information seccessfuly Send!,You will be transferred to the list of users in five seconds!',
+        title:
+          'Your Information seccessfuly Send!,You will be transferred to the list of users in five seconds!',
         icon: 'success',
         showClass: {
-          popup: 'animate__animated animate__fadeInDown'
+          popup: 'animate__animated animate__fadeInDown',
         },
         hideClass: {
-          popup: 'animate__animated animate__fadeOutUp'
-        }
-      })
+          popup: 'animate__animated animate__fadeOutUp',
+        },
+      });
 
       setTimeout(() => {
-        this.route.navigate(['/usersInformation'])
-      },5000)
+        this.route.navigate(['/usersInformation']);
+      }, 5000);
     }
   }
 }
